f44c7c2d10020c103d38cfeb87558af238a311bb
SamBat2005 <batcsg1@student.op.ac.nz>
2025-04-12 10:04:05 +1200

    feat: added expression ev to Evaluator class

--- interp.py
+++ interp.py
@@ -3,26 +3,25 @@
 ######################################
 
 import sys
-def ev(s): # Evaluation function
-    toks = s.split() # Split a line into tokens
-    stack = [] # Create an empty stack
+class Ev:
+    def ev(s): # Single expression evaluator function
+        toks = s.split() # Split a line into tokens
+        stack = [] # Create an empty stack
 
-    # Operators
-    ops = {
-        '+': lambda a, b: a + b,
-        '-': lambda a, b: a - b,
-        '*': lambda a, b: a * b,
-        '/': lambda a, b: a / b
-    }
+        # Operators
+        ops = {
+            '+': lambda a, b: a + b,
+            '-': lambda a, b: a - b,
+            '*': lambda a, b: a * b,
+            '/': lambda a, b: a / b
+        }
 
-    for tok in toks:
-        if tok.isnumeric(): stack.append(int(tok)) # Append numbers to the stack
-        elif tok in ops:
-           rhs = stack.pop()
-           lhs = stack.pop()
-           stack.append(ops[tok](lhs, rhs))
-    print(stack[0]) # Print the result
-            
-
-    #print(toks) # Print tokens
-ev(open(sys.argv[1]).read())
+        for tok in toks:
+            if tok.isnumeric(): stack.append(int(tok)) # Append numbers to the stack
+            elif tok in ops:
+                rhs = stack.pop()
+                lhs = stack.pop()
+                stack.append(ops[tok](lhs, rhs))
+        print(stack[0]) # Print the result
+        #print(toks) # Print tokens
+Ev.ev(open(sys.argv[1]).read())
