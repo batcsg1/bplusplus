22aedcb50d7f350b493cffe718c74784e079c246
SamBat2005 <batcsg1@student.op.ac.nz>
2025-04-12 09:46:34 +1200

    feat: added token for loop

--- interp.py
+++ interp.py
@@ -5,5 +5,15 @@
 import sys
 def ev(s): # Evaluation function
     toks = s.split() # Split a line into tokens
-    print(toks) # Print tokens
+    stack = [] # Create an empty stack
+
+    for tok in toks:
+        if tok.isnumeric(): stack.append(int(tok)) # Append numbers to the stack
+        elif tok == "+":
+            rhs = stack.pop() # Remove two numbers from stack
+            lhs = stack.pop()
+            stack.append(lhs + rhs) # Append result to stack
+    print(stack[0]) # Print value
+
+    #print(toks) # Print tokens
 ev(open(sys.argv[1]).read())
